3aa8c7e10fa25b463e9d9d72f8b990ec084b7a1c
pedro55562 <pedro55562@ufmg.br>
2026-01-30 14:54:39 -0300

    foi?

--- UAIbotPy/uaibot/robot/_create_rigid_body_se3.py
+++ UAIbotPy/uaibot/robot/_create_rigid_body_se3.py
@@ -11,7 +11,7 @@ from .links import *
 
 
 def _create_rigid_body_se3(htm, name, color, opacity):
-    # 1. Validações de entrada (Padrao do codigo fornecido)
+
     if not Utils.is_a_matrix(htm, 4, 4):
         raise Exception("The parameter 'htm' should be a 4x4 homogeneous transformation matrix.")
 
@@ -25,13 +25,8 @@ def _create_rigid_body_se3(htm, name, color, opacity):
     if (not Utils.is_a_number(opacity)) or opacity < 0 or opacity > 1:
         raise Exception("The parameter 'opacity' should be a float between 0 and 1.")
 
-    # 2. Definição da Tabela DH para 6-DoF (PPP + RRR)
-    # Estrutura: [Theta, d, Alpha, a, Type]
-    # Type: 1 = Prismático, 0 = Revoluto
+    # 1 = Prismático, 0 = Revoluto
     
-    # Nota sobre a cadeia PPP:
-    # Para mover em X, Y, Z usando juntas prismáticas (que normalmente atuam em Z),
-    # rotacionamos os eixos entre as juntas.
     link_info = [
         # Theta (Rot Z) [rad]
         [ np.pi/2,     0.0,     np.pi/2,    0.0,    0.0,    0.0 ],
@@ -45,30 +40,22 @@ def _create_rigid_body_se3(htm, name, color, opacity):
         # a (Trans X) [m]
         [ 0.0,     0.0,     0.0,    0.0,    0.0,    0.0 ],
 
-        # Type (1 = Prismático, 0 = Revoluto)
+        #(1 = Prismático, 0 = Revoluto)
         [ 1,       0,       1,      0,      1,      0 ]
     ]
 
     n = 6
 
-    # 3. Modelo de Colisão
-    # Cilindro simples, anexado apenas ao ultimo elo (o corpo do drone)
     col_model = [[], [], [], [], [], []]
     
-    # Ajustamos o cilindro para englobar o hexacoptero.
-    # Raio aprox 0.4m, Altura 0.15m. Rotacionado para alinhar com o plano do drone se necessario.
-    # Assumindo Z up no modelo visual.
     col_model[5].append(Cylinder(htm=Utils.roty(np.pi/2), 
                                  name=name + "_col", 
                                  radius=0.3, 
                                  height=0.20, 
-                                 color="red", 
-                                 opacity=0.3))
+                                 color="green", 
+                                 opacity=0.55))
 
-    # 4. Objetos 3D Visuais
-    # Os elos 0-4 são virtuais (sem malha), apenas o elo 5 (efetuador) tem o hexacoptero.
     
-    # Base vazia (free floating body)
     base_3d_obj = [] 
 
     link_3d_obj = [[], [], [], [], [], []]
@@ -80,7 +67,6 @@ def _create_rigid_body_se3(htm, name, color, opacity):
         htm = Utils.trn([0 , 0.0375, 0]) * Utils.rotz(-np.pi/2),
         mesh_material=MeshMaterial.create_rough_metal())
     ) 
-    # 5. Criação dos Links
     links = []
     for i in range(n):
         links.append(Link(i, link_info[0][i], link_info[1][i], link_info[2][i], link_info[3][i], link_info[4][i],
@@ -89,20 +75,18 @@ def _create_rigid_body_se3(htm, name, color, opacity):
         for j in range(len(col_model[i])):
             links[i].attach_col_object(col_model[i][j], col_model[i][j].htm)
 
-    # 6. Configuração Inicial e Limites
     q0 = [0, 000, 0, 000 , 0, 0]
-# 1 3 5
 
     large_val = 1000.0
     pi_val = np.pi
     
     joint_limits = np.matrix([
-        [-large_val, large_val], # X
-        [-pi_val, pi_val], # Y
-        [-large_val, large_val], # Z
-        [-pi_val, pi_val],       # Yaw
-        [-large_val, large_val],       # Pitch
-        [-pi_val, pi_val]        # Roll
+        [-large_val, large_val], 
+        [-pi_val, pi_val], 
+        [-large_val, large_val], 
+        [-pi_val, pi_val],       
+        [-large_val, large_val],       
+        [-pi_val, pi_val]        
     ])
 
     return base_3d_obj, links, np.identity(4), np.identity(4), q0, joint_limits
--- teste.py
+++ teste.py
@@ -6,10 +6,48 @@ from setup import *
 import itertools
 
 
+def save_q_dot_plot(q_dot_list, dt=0.01, file_name='q_dot_plot.png', save_dir=None):
+    """Salva um gráfico de q_dot ao longo do tempo na pasta do script.
+
+    q_dot_list: lista de vetores q_dot (cada um tem formato (n,1) ou (n,))
+    dt: passo de tempo entre amostras
+    file_name: nome do arquivo de saída (PNG)
+    save_dir: diretório onde salvar (por padrão, a pasta do script)
+    """
+    if len(q_dot_list) == 0:
+        return None
+
+    # Converter para matriz (timesteps, n_joints)
+    cols = [np.asarray(q).reshape(-1, 1) for q in q_dot_list]
+    data = np.hstack(cols).T
+
+    t = np.arange(data.shape[0]) * dt
+
+    plt.figure(figsize=(10, 6))
+    for j in range(data.shape[1]):
+        plt.plot(t, data[:, j], label=f'xid{j+1}')
+
+    plt.xlabel('Tempo (s)')
+    plt.ylabel('xid ')
+    plt.title('xid por tempo')
+    plt.grid(True)
+    plt.legend(loc='upper right')
+    plt.tight_layout()
+
+    if save_dir is None:
+        save_dir = os.path.dirname(__file__)
+
+    path = os.path.join(save_dir, file_name)
+    plt.savefig(path)
+    plt.close()
+    print(f'Gráfico salvo em: {path}')
+
 robot = ub.Robot.create_rigid_body_se3()
 sim = ub.Simulation(background_color = 'black')
 sim.add([robot])
 
+
+ 
 objss = []
 #Loop the link for data//
 for link in robot.links:
@@ -17,32 +55,71 @@ for link in robot.links:
     for col_obj_data in link.col_objects:
         #The actual UAIBot object (ub.Box, ub.Cylinder, etc...)    
         obj = col_obj_data[0]
+        col_obj = obj
         objss.append(obj)
 sim.add(objss)
-
 
 htm_target = ub.Utils.trn([0, 2.5, 0.7]) * robot.fkm() * ub.Utils.rot(axis=[4,3,-2], angle= 77 * np.pi / 180) 
 
 frame_target = ub.Frame(htm=htm_target)
 sim.add([frame_target])
 
-box = ub.Box(htm = ub.Utils.trn([0, 2, 0]) ,width=3, depth=0.1, height=4,)
-all_obs = []
-all_obs.append(box)
+box1 = ub.Box(htm = ub.Utils.trn([0, 2, 0.8]) ,   width=3, depth=0.1, height = 1.6  ,color='red')
+box2 = ub.Box(htm = ub.Utils.trn([0, 0, -.15]) ,width=7, depth=7  , height = 0.05  ,color='red')
+box3 = ub.Box(htm = ub.Utils.trn([0, 0, 1.45]) ,width=7, depth=7  , height = 0.05  ,color='red',opacity=0.4)
+box4 = ub.Box(htm = ub.Utils.trn([0, 3.5, 0.8]) ,   width=7, depth=0.1, height = 1.6  ,color='red')
 
+
+known_obs = [box1, box2, box3, box4]
+
+
+
+unknown_obs = []
+all_obs = known_obs + unknown_obs
 sim.add(all_obs)
 
 
 
-q_goal = robot.ikm(htm_tg=htm_target, obstacles=all_obs, no_tries = 2000, no_iter_max=4000)
 
-success1, path1, iterations1, num_tries1, planning_time1 = robot.runSE3RRT(q0 = robot.q0, q_goal=[q_goal], obstacles=all_obs)
-draw_pc(pathhh_=path1,robot = robot, sim=sim)
-print(len(path1))
-print(planning_time1)
+# Flag para gerar novo caminho
+# Se True, gera um novo caminho e salva em um arquivo
+# Se False, carrega o último caminho salvo
+
+gerar_novo_caminho = False
+
+caminho_arquivo = "ultimo_caminho.txt"
+
+def salvar_caminho(caminho, arquivo):
+    with open(arquivo, "w") as f:
+        for matriz in caminho:
+            np.savetxt(f, matriz, delimiter=",", fmt="%.4f")
+
+def carregar_caminho(arquivo):
+    caminho = []
+    with open(arquivo, "r") as f:
+        linhas = f.readlines()
+        for i in range(0, len(linhas), 6):  # Cada matriz tem 6 linhas
+            matriz = np.loadtxt(linhas[i:i+6], delimiter=",")
+            caminho.append(matriz)
+    return caminho
+
+if gerar_novo_caminho:
+    q_goal = robot.ikm(htm_tg=htm_target, obstacles=known_obs, no_tries=2000, no_iter_max=4000)
+    success1, path1, iterations1, num_tries1, planning_time1 = robot.runSE3RRT(q0=robot.q0, q_goal=[q_goal], obstacles=known_obs)
+    draw_pc(pathhh_=path1, robot=robot, sim=sim)
+    print(len(path1))
+    print(planning_time1)
+    salvar_caminho(path1, caminho_arquivo)
+else:
+    path1 = carregar_caminho(caminho_arquivo)
+    draw_pc(pathhh_=path1, robot=robot, sim=sim)
 
 
 
+htm_path = []    
+for qc in path1:
+    fkm = robot.fkm(q=qc)
+    htm_path.append(fkm)
 
 
 
@@ -50,12 +127,71 @@ i=0
 dt = 0.01
 t=0
 
-while np.linalg.norm(robot.q - q_goal) > 0.005 and t < 10:
-   t = i*dt
-   q_dot = vector_field(robot=robot, curve=path1)
-   set_configuration_speed(robot, q_dot, t, dt)
-   i+=1
+xid_list = []
+
+r , Jr = robot.task_function(htm_tg = htm_target)
+while np.linalg.norm(r) > 0.025 and t < 60:
+    t = i*dt
+
+
+    jac_geo, fkm = robot.jac_geo()
+    xid, dist, idx = robot.vector_field_SE3(
+        
+        state=fkm,            
+        curve=htm_path,       
+
+        kt1=0.75,              
+        kt2=1.0,              
+        kt3=1.0,             
+        
+        kn1=1.0,             
+        kn2=1.0,  
+        ds = dt,
+        delta = 1e-2,
+        
+    )
+    xid = np.matrix(xid).T
+    xid[0 : 3 , :] = xid[0 : 3 , :] +   ub.Utils.S(xid[3 : 6 , :]) * fkm[0 : 3 , -1]
+
+    jac, htm = robot.jac_geo()
+    
+    s = htm[0:3,-1]
+    v = jac[0:3,:]
+    w = jac[3:6,:]
+
+    Ad_obj = np.zeros((0, 6))
+    Bd_obj = np.zeros((0, 1))
+    param_eta = 1
+    param_obs_delta = 0
+    for ob in all_obs:
+
+        point_robot, point_obs, dist, _ = col_obj.compute_dist(ob, h =  0.05, eps = 0.02)
+        jac_dist = ((point_robot - point_obs).T * v + np.cross((point_robot - s ).T, (point_robot - point_obs).T)  * w) / (dist + 1e-6)
+        Ad_obj = np.vstack((Ad_obj, jac_dist))
+        Bd_obj = np.vstack((Bd_obj, -param_eta*(dist-param_obs_delta)))
+
+    
+
+    eps = 1e-6
+
+    H = 2*(1 + eps)*np.eye(6)
+    f = -2*xid.flatten()
+    
+    
+    u = ub.Utils.solve_qp(
+        H=H,
+        f=f,
+        A=Ad_obj,
+        b=Bd_obj
+        )
 
 
 
+    xid_list.append(u)
+    qdot = ub.Utils.dp_inv(jac_geo, 1e-3) @ u.reshape(6,1)
+    set_configuration_speed(robot, qdot, t, dt)
+    r , Jr = robot.task_function(htm_tg = htm_target)
+    i+=1
+
+save_q_dot_plot(xid_list, dt=dt, file_name='xid_plot.png')
 sim.save(address="/home/pedro/code/SE3_CBF/",file_name="teste_SE3")
